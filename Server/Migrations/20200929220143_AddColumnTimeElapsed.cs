c5ea77153268a45420406eb3b91a2ed768bd120d
Mikulas Bacinsky <mikulas.bacinsky@gmail.com>
2020-09-30 00:07:31 +0200

    implementation

---
+++ Server/Migrations/20200929220143_AddColumnTimeElapsed.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BomberMan.Server.Migrations
+{
+    public partial class AddColumnTimeElapsed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "TimeElapsed",
+                table: "PlayerScores",
+                nullable: false,
+                defaultValue: 0L);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TimeElapsed",
+                table: "PlayerScores");
+        }
+    }
+}
